c34223177a788eec454c2efc8ff9a5b7a57feb96
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2021-06-15 08:36:51 +0530

    added login page style

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,25 +1,31 @@
 import { Header } from "../header";
+
 import "./Login.css";
 export function Login() {
   return (
     <>
       <Header />
       <div className="inputForm">
-        <div class="form">
-          <input
-            class="form__input email"
-            type="text"
-            name="email"
-            required
-          ></input>
-          <label class="form__label">email</label>
-        </div>
-        <div class="form">
-          <input class="form__input" type="password" required></input>
-          <label class="form__label">Password</label>
-        </div>
-        <div class="btn__container">
-          <div class="btn btn--primary login ">Login</div>
+        <div className="box">
+          <div class="form">
+            <input
+              class="form__input email"
+              type="text"
+              name="email"
+              required
+            ></input>
+            <label class="form__label">email</label>
+          </div>
+          <div class="form">
+            <input class="form__input" type="password" required></input>
+            <label class="form__label">password</label>
+          </div>
+          <div class="btn__container">
+            <div class="btn btn--primary login ">Login</div>
+          </div>
+          <p className="para">
+            New to Lingokart? <span className="signup">SignUp</span>
+          </p>
         </div>
       </div>
     </>
